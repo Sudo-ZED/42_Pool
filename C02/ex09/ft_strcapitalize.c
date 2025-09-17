74297b56f9c71e741e45a5a3d1b6c5c1e49e5476
Sudo-ZED <b.tahraoui.badr@gmail.com>
2025-09-17 05:29:16 +0100

    C02

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 23:41:49 by kali              #+#    #+#             */
-/*   Updated: 2025/09/17 00:02:43 by kali             ###   ########.fr       */
+/*   Updated: 2025/09/17 05:28:22 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,29 @@ char *ft_strcapitalize(char *str)
 
     i = 0;
     while (str[i] != '\0')
-         {
-            if (i == 0 || (str[i - 1] >= '0' && str[i - 1] <= '9') ||
-                      (str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
-                      (str[i - 1] >= 'A' && str[i - 1] <= 'Z'))
-                {
-                    if (str[i] >= 'a' && str[i] <= 'z')
-                        {
-                            str[i] = str[i] - 32;
-                        }
-                    else if (str[i] >= 'A' && str[i] <= 'Z')
-                        {
-                            str[i] = str[i] + 32;
-                        }
-                }
-            i++;
+    {
+        
+        if (i == 0 || !((str[i - 1] >= '0' && str[i - 1] <= '9') ||
+                       (str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
+                       (str[i - 1] >= 'A' && str[i - 1] <= 'Z')))
+        {
+            
+            if (str[i] >= 'a' && str[i] <= 'z')
+            {
+                str[i] = str[i] - 32;
+            }
         }
-return (str);
+        else
+        {
+            
+            if (str[i] >= 'A' && str[i] <= 'Z')
+            {
+                str[i] = str[i] + 32;
+            }
+        }
+        i++;
+    }
+    return (str);
 }
 int main ()
 {
