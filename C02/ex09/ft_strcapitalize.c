bacbb171f3d86589f83e196deb0fd920ac7c8641
Sudo-ZED <b.tahraoui.badr@gmail.com>
2025-09-17 05:35:16 +0100

    v2

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 23:41:49 by kali              #+#    #+#             */
-/*   Updated: 2025/09/17 05:28:22 by kali             ###   ########.fr       */
+/*   Updated: 2025/09/17 05:33:26 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,31 @@
 
 char *ft_strcapitalize(char *str)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        
-        if (i == 0 || !((str[i - 1] >= '0' && str[i - 1] <= '9') ||
-                       (str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
-                       (str[i - 1] >= 'A' && str[i - 1] <= 'Z')))
-        {
-            
-            if (str[i] >= 'a' && str[i] <= 'z')
-            {
-                str[i] = str[i] - 32;
-            }
-        }
-        else
-        {
-            
-            if (str[i] >= 'A' && str[i] <= 'Z')
-            {
-                str[i] = str[i] + 32;
-            }
-        }
-        i++;
-    }
-    return (str);
-}
-int main ()
-{
-    char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-    printf("%s", ft_strcapitalize(str));
-    return 0;
-
-
+	i = 0;
+	while (str[i] != '\0')
+	{
+		
+		if (i == 0 || !((str[i - 1] >= '0' && str[i - 1] <= '9') ||
+					   (str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
+					   (str[i - 1] >= 'A' && str[i - 1] <= 'Z')))
+		{
+			
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		else
+		{
+			
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+		i++;
+	}
+	return (str);
 }
