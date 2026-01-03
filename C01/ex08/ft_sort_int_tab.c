5ead9ffd516567fd15e2f68492e82efa3a1b4cde
Uzal_sahn <b.tahraoui.badr@gmail.com>
2026-01-03 11:09:11 +0100

    1337 Pool C01

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -11,28 +11,43 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-
+//#include <stdio.h>
 void    ft_sort_int_tab(int *tab, int size)
 {
-    int outerindexer;
-    int inerindexer;
-    int temp;
+	int outerindexer;
+	int inerindexer;
+	int temp;
 
-    outerindexer = 0;
+	outerindexer = 0;
 
-    while(outerindexer < size - 1)
-    {
-        inerindexer = 0;
-        while(inerindexer < size - 1 - i)
-        {
-            if(tab[inerindexer] > tab[inerindexer + 1])
-            {
-                temp = tab[inerindexer];
-                tab[inerindexer] = tab[inerindexer + 1];
-                tab[inerindexer + 1] = temp;
-            }
-            inerindexer ++;
-        }
-        outerindexer++
-    }
+	while(outerindexer < size - 1)
+	{
+		inerindexer = 0;
+		while(inerindexer < size - 1 - outerindexer)
+		{
+			if(tab[inerindexer] > tab[inerindexer + 1])
+			{
+				temp = tab[inerindexer];
+				tab[inerindexer] = tab[inerindexer + 1];
+				tab[inerindexer + 1] = temp;
+			}
+			inerindexer ++;
+		}
+		outerindexer++;
+	}
 }
+/*int main(void)
+{
+	int tab[] = {5, 2, 3, 7, 1};
+	int i = 0;
+
+	int size = 5;
+
+	ft_sort_int_tab(tab, size);
+
+	for(i = 0; i < size; i++)
+	{
+		printf("%i", tab[i]);
+	}
+	return 0;
+}*/
